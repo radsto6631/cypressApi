fa98df788c241cdd88f29b5ae644528af8268ee1
Radek Stolarczyk <radsto6631@gmail.com>
2026-02-10 18:21:33 +0000

    Add sample articles to page by mocking api calls

--- cypress/e2e/workingWithAPIs.cy.js
+++ cypress/e2e/workingWithAPIs.cy.js
@@ -2,11 +2,14 @@
 
 it("first API test", () => {
 
+    cy.intercept("GET", "https://api.example.com/api/tags", { fixture: "tags.json" });
+    cy.intercept("GET", "https://api.example.com/api/articles?limit=10&offset=0", { fixture: "articles.json" });
+    
     cy.visit("/");
     cy.contains('Sign in').click();
     cy.get('input[name="email"]').type("test@example.com");
     cy.get('input[name="password"]').type("password");
     cy.get('button[type="submit"]').click();
 
-    
+
 });
