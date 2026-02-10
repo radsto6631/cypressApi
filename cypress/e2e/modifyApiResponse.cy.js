ee1d459ab54d56abf38800844d0b2323a49c0a51
Radek Stolarczyk <radsto6631@gmail.com>
2026-02-10 18:35:05 +0000

    Modify Api Response test example

---
+++ cypress/e2e/modifyApiResponse.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+it("modify API response", () => {
+
+    cy.intercept("GET", "https://api.example.com/api/tags", (req) => {
+        req.continue(res => {
+            res.body.articles[0].title = "Modified Title";
+            res.send(res.body);
+        });
+    });
+
+    cy.visit("/");
+    cy.contains('Sign in').click();
+    cy.get('input[name="email"]').type("test@example.com");
+    cy.get('input[name="password"]').type("password");
+    cy.get('button[type="submit"]').click();
+    cy.get('.article-preview').first().find('h1').should('have.text', 'Modified Title');
+});
