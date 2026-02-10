9156b283b9f3d0117468fcbfa18a72b6baf67003
Radek Stolarczyk <radsto6631@gmail.com>
2026-02-10 18:43:36 +0000

    Add tests to one file

--- cypress/e2e/workingWithAPIs.cy.js
+++ cypress/e2e/workingWithAPIs.cy.js
@@ -3,6 +3,11 @@
 it("first API test", () => {
 
     cy.intercept("GET", "https://api.example.com/api/tags", { fixture: "tags.json" });
+    //same as above
+    cy.intercept("GET", "**/tags", { fixture: "tags.json" });
+    //same as above
+    cy.intercept({method: "GET", pathname: "tags"}, { fixture: "tags.json" });
+    
     cy.intercept("GET", "https://api.example.com/api/articles?limit=10&offset=0", { fixture: "articles.json" });
     
     cy.visit("/");
@@ -10,6 +15,21 @@ it("first API test", () => {
     cy.get('input[name="email"]').type("test@example.com");
     cy.get('input[name="password"]').type("password");
     cy.get('button[type="submit"]').click();
+});
 
+it("modify API response", () => {
 
+    cy.intercept("GET", "https://api.example.com/api/tags", (req) => {
+        req.continue(res => {
+            res.body.articles[0].title = "Modified Title";
+            res.send(res.body);
+        });
+    });
+
+    cy.visit("/");
+    cy.contains('Sign in').click();
+    cy.get('input[name="email"]').type("test@example.com");
+    cy.get('input[name="password"]').type("password");
+    cy.get('button[type="submit"]').click();
+    cy.get('.article-preview').first().find('h1').should('have.text', 'Modified Title');
 });
